018f846c104eae1185bc8396aab9fe47aa160fb8
ato lash <hal_shu_sato@yahoo.co.jp>
2020-06-28 20:47:30 +0900

    Add displaying now date & time

--- RBAT/MainWindow.xaml.cs
+++ RBAT/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace RBAT
 {
@@ -20,9 +21,45 @@ namespace RBAT
     /// </summary>
     public partial class MainWindow : Window
     {
+        /// <summary>
+        /// 時刻表示用タイマー
+        /// </summary>
+        private DispatcherTimer timer;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            //タイマー生成
+            timer = CreateTimer();
+            timer.Start();
         }
+
+        /// <summary>
+        /// タイマー生成処理
+        /// </summary>
+        /// <returns>生成したタイマー</returns>
+        private DispatcherTimer CreateTimer()
+        {
+            // タイマー生成（優先度はアイドル時に設定）
+            var t = new DispatcherTimer(DispatcherPriority.SystemIdle);
+
+            // タイマーイベントの発生間隔を300ミリ秒に設定
+            t.Interval = TimeSpan.FromMilliseconds(100);
+
+            // タイマーイベントの定義
+            t.Tick += (sender, e) => {
+                // タイマーイベント発生時の処理をここに書く
+
+                // 現在の年月日曜日をテキストに設定
+                nowDate.Content = DateTime.Now.ToString("yyyy/MM/dd（ddd）");
+                // 現在の時分秒をテキストに設定
+                nowTime.Content = DateTime.Now.ToString("HH:mm:ss");
+            };
+
+            // 生成したタイマーを返す
+            return t;
+        }
+
     }
 }
